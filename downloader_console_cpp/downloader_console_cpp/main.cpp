b11b834747a6a22054d05b55302dca574bc0b41e
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2024-05-27 00:47:30 +0330

    write a simple code to main.cpp

--- downloader_console_cpp/downloader_console_cpp/main.cpp
+++ downloader_console_cpp/downloader_console_cpp/main.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
+string dl_link;
+
 int main()
 {
+	std::cout << "Hello! Welcome to File Downloader. \n" << endl;
+	std::cout << "Please Copy your File Here: ";
+	std::cin >> dl_link;
+	std::cout << "\n";
+	std::cout << "Your Link is: " << dl_link;
 	return 0;
 }
