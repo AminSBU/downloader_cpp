a7b70736458e96f2c10817a31c2d78420df902fb
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2024-05-29 22:54:24 +0330

    consider menu to insert download file address, use curl function to download

--- downloader_console_cpp/downloader_console_cpp/main.cpp
+++ downloader_console_cpp/downloader_console_cpp/main.cpp
@@ -5,23 +5,46 @@ using namespace std;
 
 string dl_link;
 
+int num_menu;
+
 int main()
 {
-    std::string url = "http://example.com/file.txt";
-    std::string output = "downloaded_file.txt";
+    std::cout << "Welcome to CPP Downloader!" << std::endl;
+    std::cout << "1. Download Files ..." << std::endl;
+    std::cout << "2. About Application ...\n" << std::endl;
+    std::cout << "*************************\n";
+    std::cout << "enter Your choose: ";
+    std::cin >> num_menu;
 
-    // Use the Windows command prompt to download the file
-    std::string command = "curl -o " + output + " " + url;
+    switch (num_menu)
+    {
+    case 1:
+    {
+        std::cout << "paste your download link: " << std::endl;
+        std::cin >> dl_link;
 
-    // Execute the command using system call
-    int result = system(command.c_str());
+        std::string url = dl_link;
+        std::string output = "downloaded_file.mp3";
 
-    if (result == 0) {
-        std::cout << "File downloaded successfully." << std::endl;
+        // Use the Windows command prompt to download the file
+        std::string command = "curl -o " + output + " " + url;
+
+        // Execute the command using system call
+        int result = system(command.c_str());
+
+        if (result == 0) {
+            std::cout << "File downloaded successfully." << std::endl;
+        }
+        else {
+            std::cout << "Error downloading file." << std::endl;
+        }
     }
-    else {
-        std::cout << "Error downloading file." << std::endl;
+    break;
+
+    default:
+        break;
     }
+    
 
     return 0;
 }
